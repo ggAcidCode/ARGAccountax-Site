9719e4f2d0d0958915849491a17210d7b010611c
Gar <gartheepan@gmail.com>
2026-02-22 15:09:55 -0500

    Added google verification

--- ARGAccountax-Site/app/layout.tsx
+++ ARGAccountax-Site/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://taxraj.com'),
+  verification: {
+    google: 'x5kkPOLcK7f6G_M4HfEDdWoG--6n8AQwfpiGht-sGes'
+  },
 };
 
 export default function RootLayout({
